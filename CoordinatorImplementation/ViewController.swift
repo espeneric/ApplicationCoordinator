e95a7dd01152ba44ace99c667c4cd9fea99f21e9
Sergheev Andrian <sergheev@icloud.com>
2019-01-25 19:53:38 +0100

    Initial Commit

---
+++ CoordinatorImplementation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorImplementation
+//
+//  Created by Andrian Sergheev on 2019-01-25.
+//  Copyright © 2019 Andrian Sergheev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
